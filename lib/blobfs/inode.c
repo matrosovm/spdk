768fbaee0a0873f9c0a676c477feab910278eafc
matrosovm <2831891@gmail.com>
2023-05-29 10:25:00 +0300

    Some inode fixes

--- lib/blobfs/inode.c
+++ lib/blobfs/inode.c
@@ -6,10 +6,24 @@
 #include <sys/types.h>
 #include "blobfs.h"
 
+struct spdk_inode {
+    mode_t mode;
+    uid_t uid;
+    gid_t gid;
+    time_t atime;
+    time_t mtime;
+    time_t ctime;
+    off_t size;
+    int blocks_count;
+    uint64_t data[32];
+};
+
 struct cache_entry {
     uint64_t key;
     void *data;
     time_t last_access;
+    struct spdk_inode *inode;
+    uint32_t hash;    
     struct cache_entry *prev;
     struct cache_entry *next;
 };
@@ -20,6 +34,34 @@ struct cache {
     struct cache_entry *head;
     struct cache_entry *tail;
 };
+
+struct superblock {
+    uint32_t magic;
+    uint32_t version;
+    uint64_t size;
+    uint64_t block_count;
+    uint64_t block_size;
+    uint64_t inode_count;
+    uint64_t inode_table_block;
+    uint64_t data_block;
+    uint64_t data_block_count;
+    uint64_t free_block_count;
+    uint64_t free_block_bitmap;
+    uint64_t dir_block;
+    uint64_t inode_blob;
+    uint64_t inode_channel;
+    uint64_t last_ino;
+    uint64_t first_ino;
+    uint64_t inode_table_offset;
+    uint64_t inode_cache;
+};
+
+
+struct blobfs {
+    struct spdk_file *spdk_blob_fs;
+    struct superblock *sb;
+};
+
 
 static struct cache_entry*
 __cache_find_entry(struct cache *cache, uint64_t key)
@@ -49,11 +91,11 @@ __cache_find_entry_by_data(struct cache *cache, void *data)
     return NULL;
 }
 
-static void *
+static void*
 __cache(struct cache *cache, uint64_t key)
 {
     struct cache_entry *entry;
-    entry = cache_find_entry(cache, key);
+    entry = __cache_find_entry(cache, key);
 
     if (entry) {
         entry->last_access = time(NULL);
@@ -68,80 +110,194 @@ __cache_update(struct cache *cache, void *data)
 {
     struct cache_entry *entry;
 
-    entry = cache_find_entry_by_data(cache, data);
+    entry = __cache_find_entry_by_data(cache, data);
     if (entry) {
         entry->last_access = time(NULL);
     }
 }
 
-static struct blobfs_inode*
-__spdk_cache_lookup_inode(struct *blobfs, uint64_t ino)
+void* 
+__cache_lookup(struct cache* cache, uint64_t key)
 {
-    struct blobfs_inode *inode;
+    struct cache_entry* entry = cache->head;
 
-    inode = (struct blobfs_inode *)cache_lookup(blobfs->inode_cache, ino);
+    while (entry != NULL) {
+        if (entry->key == key) {
+            entry->last_access = time(NULL);
+
+            if (entry != cache->head) {
+                entry->prev->next = entry->next;
+                if (entry->next != NULL) {
+                    entry->next->prev = entry->prev;
+                } else {
+                    cache->tail = entry->prev;
+                }
+                entry->next = cache->head;
+                entry->prev = NULL;
+                cache->head->prev = entry;
+                cache->head = entry;
+            }
+
+            return entry->data;
+        }
+
+        entry = entry->next;
+    }
+
+    return NULL;
+}
+
+int 
+__cache_update(struct cache* cache, uint64_t key, void* data)
+{
+    struct cache_entry* entry = cache->head;
+
+    while (entry != NULL) {
+        if (entry->key == key) {
+            entry->data = data;
+            entry->last_access = time(NULL);
+
+            if (entry != cache->head) {
+                entry->prev->next = entry->next;
+                if (entry->next != NULL) {
+                    entry->next->prev = entry->prev;
+                } else {
+                    cache->tail = entry->prev;
+                }
+                entry->next = cache->head;
+                entry->prev = NULL;
+                cache->head->prev = entry;
+                cache->head = entry;
+            }
+
+            return 0;
+        }
+
+        entry = entry->next;
+    }
+
+    return -1;
+}
+
+
+static struct spdk_inode*
+__spdk_cache_lookup_inode(struct blobfs* blobfs, uint64_t ino)
+{
+    struct spdk_inode *inode;
+
+    inode = (struct spdk_inode *)__cache_lookup(blobfs->sb->inode_cache, ino);
     if (inode) {
-        cache_update(blobfs->inode_cache, inode);
+        __cache_update(blobfs->sb->inode_cache, inode);
     }
 
     return inode;
 }
 
-static struct blobfs_inode *
+
+#define SPDK_CACHE_NUM_BUCKETS 1024
+static
+uint32_t __spdk_cache_inode_hash(uint64_t ino)
+{
+    // using fnv hashing
+    uint32_t hash = 538;
+    uint8_t *p = (uint8_t *)&ino;
+    int i;
+
+    for (i = 0; i < sizeof(ino); i++) {
+        hash = ((hash << 5) + hash) + (*p++);
+    }
+
+    return hash % SPDK_CACHE_NUM_BUCKETS;
+}
+
+
+static
+void __spdk_cache_add_inode(struct blobfs *blobfs, struct spdk_inode *inode)
+{
+    uint32_t hash = __spdk_cache_inode_hash(inode->uid);
+    struct cache_entry *entry = calloc(1, sizeof(struct cache_entry*));
+
+    if (!entry) {
+        free(inode);
+        return;
+    }
+
+    entry->inode = inode;
+    entry->hash = hash;
+
+    pthread_mutex_lock(&blobfs->spdk_blob_fs->lock);
+    TAILQ_INSERT_TAIL(&blobfs->cache[hash], entry, link);
+    pthread_mutex_unlock(&blobfs->spdk_blob_fs->lock);
+}
+
+
+static struct spdk_inode*
 __spdk_cache_get_inode(struct blob *blobfs, uint64_t ino)
 {
-    struct blobfs_inode *inode;
+    struct spdk_inode *inode;
 
-    inode = blobfs_cache_lookup_inode(blobfs, ino);
+    inode = __spdk_cache_lookup_inode(blobfs, ino);
     if (inode) {
         return inode;
     }
 
-    inode = blobfs_read_inode(blobfs, ino);
+    inode = __spdk_read_inode(blobfs, ino);
     if (!inode) {
         return NULL;
     }
 
-    blobfs_cache_add_inode(blobfs, inode);
+    __spdk_cache_add_inode(blobfs, inode);
 
     return inode;
 }
 
 static uint64_t
-__blobfs_inode_offset(struct blobfs *blobfs, uint64_t ino)
+__spdk_inode_offset(struct blobfs *blobfs, uint64_t ino)
 {
     uint64_t offset;
-    offset = blobfs->superblock.inode_table_offset +
-             (ino - blobfs->superblock.first_ino) * sizeof(struct spdk_inode);
+    offset = blobfs->sb->inode_table_offset +
+             (ino - blobfs->sb->first_ino) * sizeof(struct spdk_inode);
 
     return offset;
 }
 
-static struct blobfs_inode *
-blobfs_read_inode(struct blobfs *blobfs, uint64_t ino{
-    struct blobfs_inode *inode;
+static void 
+__spdk_read_inode_complete(struct blobfs *blobfs, void *cb, int bserrno)
+{
+    struct spdk_inode *inode = cb;
+
+    if (bserrno) {
+        free(inode);
+        return;
+    }
+
+    __spdk_cache_add_inode(blobfs, inode);
+}
+
+
+static struct spdk_inode*
+__spdk_read_inode(struct blobfs *blobfs, uint64_t ino)
+{
+    struct spdk_inode *inode;
     uint64_t offset;
 
-    if (ino < blobfs->superblock.first_ino || ino > blobfs->superblock.last_ino) {
+    if (ino < blobfs->sb->first_ino || ino > blobfs->sb->last_ino) {
         return NULL;
     }
 
-    inode = blobfs_cache_get_inode(blobfs, ino);
+    inode = __spdk_cache_get_inode(blobfs, ino);
     if (inode) {
         return inode;
     }
 
-    offset = blobfs_inode_offset(blobfs, ino);
-    inode calloc(1, sizeof(*inode));
+    offset = __spdk_inode_offset(blobfs, ino);
+    inode = calloc(1, sizeof(struct spdk_inode));
     if (!inode) {
         return NULL;
     }
 
-    if (spdk_blob_io_read(blobfs->inode_blob, blobfs->inode_channel, inode,
-                          sizeof(*inode), blobfs_read_inode_complete, inode)) {
-        free(inode);
-        return NULL;
-    }
+    spdk_blob_io_read(blobfs->spdk_blob_fs, blobfs->sb->inode_channel, inode, 
+                        offset, sizeof(*inode), __spdk_read_inode_complete, inode);
 
     return inode;
 }
